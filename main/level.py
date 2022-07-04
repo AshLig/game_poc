bdd7d2f937270cccc9a9a0ad516eda7b7d83f7ac
Ashwani Ligin <ashwaniligin@CTM-MAC-040298.bgl.financial.local>
2022-07-04 17:27:09 +0100

    added camera movement

--- main/level.py
+++ main/level.py
@@ -11,7 +11,7 @@ class Level:
         
         self.display_surface = pygame.display.get_surface() #gets the display surface from anywhere in the code
 
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
 
         self.create_map()
@@ -28,5 +28,25 @@ class Level:
     
     def run(self):
         #update and draw game
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+
+class YSortCameraGroup(pygame.sprite.Group): #class - camera being sorted by the Y co-ordinates
+    def __init__(self):
+
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2 # to get player in middle of screen
+        self.half_height = self.display_surface.get_size()[1] // 2 # to get player in middle of screen
+        self.offset = pygame.math.Vector2()  # vector gives control of where sprites are drawn
+    
+    def custom_draw(self,player):
+
+        #getting offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
